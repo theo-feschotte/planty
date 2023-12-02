7b482f99b4c04ebf96f85c8e4d200f0d095f35d2
Théo Feschotte <theofeschotte@gmail.com>
2023-12-02 18:12:54 +0100

    Désactivation WordFence

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1701995038;s:7:"authKey";s:64:"8[(@p*/%7j~*Y&$7]!B|Q#q:#N 8waHBI}bv`/G8<&V$?V7y(p`FCaw3y/2b<3{3";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:802;}
