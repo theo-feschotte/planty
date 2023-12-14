4981552233e58440b7d1f8c578b79481aa1eebee
Théo Feschotte <theofeschotte@gmail.com>
2023-12-14 04:45:57 +0100

    Styles bloc c-flavours

--- wp-content/themes/planty/template-accueil.php
+++ wp-content/themes/planty/template-accueil.php
@@ -13,7 +13,7 @@ include "variables-accueil.php";
     <div class="entry-content">
 
         <section class="c-hero" style="background-color:<?php echo( $hero_background_color ); ?>;">
-            <div class="o-wrapper">
+            <div class="o-wrapper-xl">
                 <div class="c-hero__title">
                     <?php echo( $hero_title ? $hero_title : the_title() ); ?>
                 </div>
@@ -32,33 +32,35 @@ include "variables-accueil.php";
         </section>
         
         <section class="c-flavours" style="background-color:<?php echo( $flavours_background_color ); ?>;">
-            <div class="o-wrapper">
+            <div class="o-wrapper-xl">
                 <div class="c-flavours__title">
                     <?php echo( $flavours_title ); ?>
                 </div>
                 <ul class="c-flavours__list">
                     <?php foreach ($flavours_list as $flavour) {
                         echo( '<li class="c-flavours__list-item">' );
-                            echo( '<p>'.$flavour["name"].'</p>' );
+                            echo( '<p class="c-flavours__list-name">'.$flavour["name"].'</p>' );
                             echo( '<img class="c-flavours__list-image" src="'.$flavour["image"]["url"].'" alt="'.$flavour["image"]["alt"].'">' );
                         echo( '</li>' );
                     } ?>
                 </ul>
-                <?php echo( '<a class="c-flavours__link button-link" href="'.$flavours_link["url"].'" target="'.$flavours_link["target"].'">'.$flavours_link["title"].'</a>' ); ?>
+                <?php echo( '<a class="c-flavours__link btn-link" href="'.$flavours_link["url"].'" target="'.$flavours_link["target"].'">'.$flavours_link["title"].'</a>' ); ?>
             </div>
         </section>
 
         <section class="c-testimonials" style="background-color:<?php echo( $testimonials_background_color ); ?>;">
-            <div class="o-wrapper">
+            <div class="o-wrapper-xl">
                 <div class="c-testimonials__title">
                     <?php echo( $testimonials_title ); ?>
                 </div>
                 <ul class="c-testimonials__list">
                     <?php foreach ($testimonials_list as $testimonial) {
                         echo( '<li class="c-testimonials__list-item">' );
-                            echo( '<p class="c-testimonials__list-name">'.$testimonial["name"].'</p>' );
-                            echo( '<p class="c-testimonials__list-texts">'.$testimonial["texts"].'</p>' );
                             echo( '<img class="c-testimonials__list-image" src="'.$testimonial["image"]["url"].'" alt="'.$testimonial["image"]["alt"].'">' );
+                            echo( '<div class="c-testimonials__list-texts">' );
+                                echo( '<p class="c-testimonials__list-texts__name">'.$testimonial["name"].'</p>' );
+                                echo( '<p class="c-testimonials__list-texts__text">'.$testimonial["texts"].'</p>' );
+                            echo( '</div>' );
                         echo( '</li>' );
                     } ?>
                 </ul>
