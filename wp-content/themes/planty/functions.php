c753b48f24c1b4962d43458d6a2008c291d44b60
Théo Feschotte <theofeschotte@gmail.com>
2023-12-16 22:47:20 +0100

    Only display menu-admin-link for logged_in_users

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -8,3 +8,19 @@ function wpm_enqueue_styles() {
     // Components stylesheet - SASS compiled
     wp_enqueue_style( 'sass-style', get_stylesheet_directory_uri() . '/dist/css/styles.css' );
 }
+
+// Only display 'Admin' link in the menu for logged_in users
+add_filter( 'wp_nav_menu_objects', 'custom_filter_menu', 10, 2 );
+function custom_filter_menu( $sorted_menu_objects, $args ) {
+    if ($args->menu == 'menu_principal') {
+        return $sorted_menu_objects;
+    };
+    foreach ($sorted_menu_objects as $key => $menu_object) {
+        if ($menu_object->title == 'Admin') {
+            if (! is_user_logged_in()) {
+                unset($sorted_menu_objects[$menu_object->menu_order]);
+            };
+        };
+    };
+    return $sorted_menu_objects;
+};
