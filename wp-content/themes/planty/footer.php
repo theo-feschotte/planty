eeb88e3baeb8b669519d8f1d8bb9994a2fc2031a
Théo Feschotte <theofeschotte@gmail.com>
2023-12-17 18:19:39 +0100

    Ajout footer bg-image et bg-color

--- wp-content/themes/planty/footer.php
+++ wp-content/themes/planty/footer.php
@@ -1,18 +1,31 @@
+<?php
+
+// FOOTER IMAGE
+$footer_image = get_field( "footer__image" );
+$footer_background_color = get_field( "footer_background_color" );
+
+?>
             </main>
         </div>
 
-        <footer id="footer">
-            <?php
-                wp_nav_menu([
-                    'theme_location' => 'menu-footer',
-                    'container' => '',
-                    'menu_class' => 'menu-list',
-                ]);
-            ?>
+        <footer id="footer" class="c-footer">
+
+            <?php echo( $footer_image ? '<div class="c-footer__bg-img" style="background-image:url('.$footer_image["url"].'); background-color:'.$footer_background_color.';"></div>' : "" ); ?>
+            
+            <div class="o-wrapper-xl">
+                <?php
+                    wp_nav_menu([
+                        'theme_location' => 'menu-footer',
+                        'container' => '',
+                        'menu_class' => 'menu-list',
+                    ]);
+                ?>
+            </div>
+
         </footer>
-
+    
     </div>
-
+    
     <?php wp_footer(); ?>
 
 </body>
