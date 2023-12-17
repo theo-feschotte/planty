8758c5ce888dcf803438ccf4e50f1bc835195f54
Théo Feschotte <theofeschotte@gmail.com>
2023-12-17 18:19:58 +0100

    Update variables-precommander.php

--- wp-content/themes/planty/variables-precommander.php
+++ wp-content/themes/planty/variables-precommander.php
@@ -10,24 +10,3 @@ $form_informations_title = get_field( "precommander__form_informations_title" );
 $form_address_title = get_field( "precommander__form_address_title" );
 $form_shortcode = do_shortcode( get_field( "precommander__form_shortcode" ) );
 $form_background_color = get_field( "precommander__form_background_color" );
-
-// $summary_title = get_field( "precommander__summary_title" );
-// $summary_background_color = get_field( "precommander__summary_background_color" );
-// $summary_list = [
-//     [
-//         "name" => get_field( "precommander__summary_flavour_title_first" ),
-//         "image" => get_field( "precommander__summary_flavour_image_first" ),
-//     ],
-//     [
-//         "name" => get_field( "precommander__summary_flavour_title_second" ),
-//         "image" => get_field( "precommander__summary_flavour_image_second" ),
-//     ],
-//     [
-//         "name" => get_field( "precommander__summary_flavour_title_third" ),
-//         "image" => get_field( "precommander__summary_flavour_image_third" ),
-//     ],
-//     [
-//         "name" => get_field( "precommander__summary_flavour_title_fourth" ),
-//         "image" => get_field( "precommander__summary_flavour_image_fourth" ),
-//     ],
-// ];
